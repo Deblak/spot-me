a04590d4fa930a7d83520f0d77225babafe41269
Deblak <deblak@mailo.com>
2024-06-24 16:54:02 +0200

    add DDL and DML scripts #1

---
+++ spot-me-database/Scripts/data.dml.sql
@@ -0,0 +1,6 @@
+DELETE FROM t_spots;
+
+INSERT INTO t_spots (spot_name, spot_lat, spot_lng, spot_desc) VALUES 
+('Yann Couvreur', 48.8481804, 2.4340378, 'Patisserie en click and collect'),
+('Chicken Story', 48.8555617, 2.4779655, 'Restauration rapide');
+
